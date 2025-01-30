95d4bf02e2a9c087dc1defd80a62d4c99a431e08
Christian <christian_96_09@hotmail.com>
2025-01-30 12:55:30 -0300

    correccion calculo de valor tec y paridad

--- app/src/main/java/com/markets/argyview/activos/Bono.kt
+++ app/src/main/java/com/markets/argyview/activos/Bono.kt
@@ -6,14 +6,13 @@ import com.google.gson.annotations.Expose
 import org.decampo.xirr.Transaction
 import org.decampo.xirr.Xirr
 import java.time.LocalDate as ldJT
-import java.time.format.DateTimeFormatter as dtfJT
-import java.time.temporal.ChronoUnit as cuJT
 import kotlin.math.pow
 
 
 import org.threeten.bp.LocalDate
 import org.threeten.bp.format.DateTimeFormatter
 import org.threeten.bp.temporal.ChronoUnit
+import kotlin.math.abs
 
 open class Bono(ticker: String, precio: Double, moneda: String, dif: Double,
                 @Expose val flujo : List<PagoBono>) : Activo(ticker, precio, moneda, dif) {
@@ -38,16 +37,22 @@ open class Bono(ticker: String, precio: Double, moneda: String, dif: Double,
 
     fun getValTec():Double{
         val hoy = LocalDate.now()
-        val ultCupon: PagoBono = try {
+
+        val proxCupones : List<PagoBono> = this.flujo.filter { it.fecha > hoy }
+        val proxCupon : PagoBono = proxCupones.first()
+
+        val ultCupon : PagoBono = try {
             this.flujo.filter { it.fecha < hoy }.last()
         }catch (e:NoSuchElementException){
+            val difMeses = abs(ChronoUnit.MONTHS.between(proxCupon.fecha, proxCupones[1].fecha))
+            PagoBono(proxCupon.fecha.minusMonths(difMeses),0.0,0.0)
+        }catch (e:ArrayIndexOutOfBoundsException){
             PagoBono(LocalDate.now(),0.0,0.0)
         }
-        val proxCupon: PagoBono = this.flujo.filter { it.fecha > hoy }.first()
 
-        val diasPasados = ChronoUnit.DAYS.between(ultCupon.fecha, hoy)
-        val diasTotales = ChronoUnit.DAYS.between(ultCupon.fecha, proxCupon.fecha)
-        val intCorrido = diasPasados * proxCupon.cupon /diasTotales
+        val diasPasados = abs(ChronoUnit.DAYS.between(ultCupon.fecha, hoy))
+        val diasTotales = abs(ChronoUnit.DAYS.between(ultCupon.fecha, proxCupon.fecha))
+        val intCorrido = diasPasados * proxCupon.cupon / diasTotales
 
         return getVN() + intCorrido
     }
